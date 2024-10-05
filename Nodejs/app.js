a8cb428e98e4ca7240b3a74f18257c94346b161a
arunachalam2311 <arunachalam.v@doodleblue.in>
2024-10-05 17:05:36 +0530

    sd

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -3,8 +3,8 @@ const StudentModel = require("./src/model/student.schema")
 const app = express()
 const connectDb = require("./src/config/dbconfig")
 app.use(express.json())
-const bcrypt =  require("bcryptjs")
-const jwt =  require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 const port = 7000
 connectDb()
@@ -15,24 +15,23 @@ app.get("/", (req, res) => {
 
 
 
-
 app.post("/register", async (req, res, nxt) => {
 
     try {
 
         let body = req.body
 
-        const salt = await  bcrypt.genSalt(10)
+        const salt = await bcrypt.genSalt(10)
 
-        const hashpassword =  await bcrypt.hash(body.password,salt)
+        const hashpassword = await bcrypt.hash(body.password, salt)
 
-body.password = hashpassword
+        body.password = hashpassword
 
         let stu = new StudentModel(body)
         await stu.save()
 
 
-        let da =  StudentModel.findOne({email:body.email})
+        let da = StudentModel.findOne({ email: body.email })
 
         res.send({
             status: 400,
@@ -48,52 +47,51 @@ body.password = hashpassword
 
 })
 
-app.post("/login" , async(req,res,nxt)=>{
+app.post("/login", async (req, res, nxt) => {
 
-    try{
+    try {
 
 
-        let {email,password} =  req.body
+        let { email, password } = req.body
 
-        let studentdata =  await StudentModel.findOne({email:email})
+        let studentdata = await StudentModel.findOne({ email: email })
 
 
 
         console.log(studentdata);
-        
-        if(!studentdata) {
+
+        if (!studentdata) {
             res.send({
-                status:400,
-                msg:"invalid"
+                status: 400,
+                msg: "invalid"
             })
         }
 
 
 
-        const invalidpass =  await bcrypt.compare(password,studentdata.password)
+        const invalidpass = await bcrypt.compare(password, studentdata.password)
 
-        if(!invalidpass){
+        if (!invalidpass) {
 
-          return  res.send({
-                status:400,
-                msg:"invalid username or password"
+            return res.send({
+                status: 400,
+                msg: "invalid username or password"
             })
 
         }
 
 
-        const token =  jwt.sign({userid :studentdata._id},"weekendnodejs",{
+        const token = jwt.sign({ userid: studentdata._id }, "weekendnodejs", {
             expiresIn: "1d"
-        } )
+        })
 
 
 
         res.send({
-            msg:"sucess logined",
-            data:studentdata,
-            token :token
+            msg: "sucess logined",
+            // data: studentdata,
+            token: token
         })
-        
 
 
 
@@ -106,13 +104,85 @@ app.post("/login" , async(req,res,nxt)=>{
 
 
 
-    }catch(err){
-      console.log(err);
-      nxt(err)
-      
+
+    } catch (err) {
+        console.log(err);
+        nxt(err)
+
     }
 
 })
+
+
+
+
+
+
+
+// admin
+
+
+app.get("/getallstudent", async (req, res, nxt) => {
+
+
+    try {
+
+
+        let data = await StudentModel.find().select("name email")
+
+        res.send({
+            data: data,
+
+        })
+
+
+    } catch (err) {
+        nxt(err)
+    }
+
+})
+
+
+
+
+app.get("/getbyid", async (req, res, nxt) => {
+
+    try {
+
+        console.log(req.headers.authorization);
+
+        let tokennn = req.headers.authorization.split(" ")
+
+
+
+        let decode = jwt.verify(tokennn[1], "weekendnodejs")
+
+        console.log(decode);
+        // console.log(req.quer/y.id);
+
+        let Studentdata = await StudentModel.findById(decode.userid).select("-password")
+
+        res.send({
+            data: Studentdata
+        })
+
+    } catch (err) {
+
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
